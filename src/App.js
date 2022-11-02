02fa2e904a94de777f04bcbd52a1e1dbe9f49489
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-11-03 00:36:32 +0600

    added final reconize

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as speech from "@tensorflow-models/speech-commands";
+import React, { useEffect, useState } from 'react';
+const App = () => {
 
-function App() {
+
+
+  const [model, setModel] = useState(null);
+  const [action, setAction] = useState(null);
+  const [labels, setLabels] = useState(null);
+
+
+  const loadModel = async () => {
+    const reconiger = await speech.create("BROWSER_FFT");
+    console.log("Modal Loaded")
+    await reconiger.ensureModelLoaded()
+    console.log(reconiger.wordLabels())
+    setModel(reconiger)
+    setLabels(reconiger.wordLabels());
+  }
+
+
+
+  useEffect(() => { loadModel() }, []);
+
+
+  function argMax(arr) {
+    return arr.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+  }
+
+  const reconigeCommands = async () => {
+    console.log('listening for commands')
+    model.listen(result => {
+      console.log(result)
+      setAction(labels[argMax(Object.values(result.scores))])
+      console.log(action)
+    }, { includeSpectrogram: true, probabilityThreshold: 0.9 })
+    setTimeout(() => model.stopListening(), 10e3);
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +49,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={reconigeCommands}>Command</button>
+        {action ? <div>{action}</div> : <div>No action Detected</div>}
+
       </header>
     </div>
   );
